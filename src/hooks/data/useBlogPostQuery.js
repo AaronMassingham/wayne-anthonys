a0b9611d7ed721279402fea17c1bed2c8363c76e
AaronM <aaron.massingham@outlook.com>
2022-08-19 19:59:37 +0100

    test

--- src/hooks/data/useBlogPostQuery.js
+++ src/hooks/data/useBlogPostQuery.js
@@ -2,20 +2,20 @@ import { useStaticQuery, graphql } from "gatsby"
 
 export const useBlogPostQuery = () => {
 
-    const data = useStaticQuery(graphql`
-    query {
-        allWpPost {
-            edges {
-                node {
-                    id
-                    slug
-                    title
-                    excerpt
+    const data = useStaticQuery(
+    graphql`
+        query {
+            allWpPost {
+                edges {
+                    node {
+                        id
+                        slug
+                        title
+                        excerpt
+                    }
                 }
             }
-        }
-    }  
-      
-`)
+        }      
+    `)
     return data
 }
