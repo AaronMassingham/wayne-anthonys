bc5bf685a359ac1cb1cd4e002a5602c0f48950c4
AaronM <aaron.massingham@outlook.com>
2022-08-19 20:19:00 +0100

    updated

--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -1,42 +1,39 @@
 import React from 'react'
-//import { graphql } from "gatsby"
+import { graphql } from "gatsby"
 
 //Layout Components
 import Layout from '../components/layout/Layout'
 import SolidBackground from '../components/blocks/SolidBackground'
 
 
-export default function BlogPost({/* data */}) {
+export default function BlogPost({ data }) {
 
-    //const post = data.wpPost
+    const post = data.wpPost
 
-    //const styles = {
-    //    lineHeight: .85,
-    //    marginTop: '2rem'
-    //}
+    const styles = {
+        lineHeight: .85,
+        marginTop: '2rem'
+    }
 
     return (
         <Layout>
             <SolidBackground>
-                {/*
                 <span>Posted <em>{ post.date }</em></span>
                 <h1 className='small' style={styles}>{ post.title }</h1>
                 <div dangerouslySetInnerHTML={{ __html: post.content }} />
-                */}
-                posts
             </SolidBackground>
         </Layout>
     )
 }
 
-//export const query = graphql`
-//    query ($slug: String!) {
-//        wpPost(slug: {eq: $slug}) {
-//            slug
-//            title
-//            date(formatString: "DD MMMM, YYYY")
-//            content
-//        }
-//    }
-//  `
+export const query = graphql`
+    query ($slug: String!) {
+        wpPost(slug: {eq: $slug}) {
+            slug
+            title
+            date(formatString: "DD MMMM, YYYY")
+            content
+        }
+    }
+  `
 
