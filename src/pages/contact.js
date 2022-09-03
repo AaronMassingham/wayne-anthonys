134a2d6893f93952f823421761df32cb35e1c9a4
AaronM <aaron.massingham@outlook.com>
2022-09-03 14:40:28 +0100

    Map Updates

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
+//Font Awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+
 //Layout Components
 import Layout from '../components/layout/Layout'
 import SolidBackground from '../components/blocks/SolidBackground'
@@ -31,8 +35,13 @@ export default function Contact() {
       <SolidBackground>
         <div className='constrained-width'>
             <h2 className='withBorders'>Where we at?</h2>
+            <p>LPR Business Park, Halifax Road, Hipperholme, Halifax, HX3 8ER</p>
+
         </div>
         <Map/>
+        <div className='text-center' style={{paddingTop: '2rem'}}>
+          <FontAwesomeIcon icon={ faLocationDot }/> <a style={{color:'white'}} target="_blank" rel="noreferrer" href='https://www.google.com/maps/place/Wayne+Anthonys+Executive+Barbers/@53.725962,-1.8180867,17z/data=!3m1!4b1!4m5!3m4!1s0x487be7812a041a41:0x28dfce5b57b9c3c3!8m2!3d53.7259609!4d-1.8160168'>Get Directions</a>
+        </div>
       </SolidBackground>
     </Layout>
   )
